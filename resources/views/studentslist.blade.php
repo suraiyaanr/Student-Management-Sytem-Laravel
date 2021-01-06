958893bbe9ffe86c15d0b1df7b2a4e61b2dde5ce
Suraiya Anuar <suraiyaanuar@gmail.com>
2021-01-06 15:37:18 +0800

    first commit

---
+++ resources/views/studentslist.blade.php
@@ -0,0 +1,57 @@
+
+<div class="card mb-3">
+    <img src="https://www.engage2serve.com/blog/wp-content/uploads/2019/04/header.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Students list</h5>
+
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">IC No.</th>
+                <th scope="col">First name</th>
+                <th scope="col">Second Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Speciality</th>
+                <th scope="col">Update</th>
+                <th scope="col">Delete</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->cne }}</td>
+                    <td>{{ $student->firstName }}</td>
+                    <td>{{ $student->secondName }}</td>
+                    <td>{{ $student->age }}</td>
+                    <td>{{ $student->speciality }}</td>
+                    <td align="center">
+                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg></a>
+                    </td>
+                     <td align="center">
+                        <form action="{{ url('/destroy/'.$student->id) }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('post') }}
+                            <button type="submit" class="btn btn-sm btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></button>
+                        </form>
+                    </td>
+
+
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+
+
+
